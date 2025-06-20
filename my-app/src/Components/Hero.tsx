57b6db8c6034f8f8a10c265520b934587b747e10
daman599 <damanpreetk117@gmail.com>
2025-06-20 19:05:00 +0530

    resume added

--- my-app/src/Components/Hero.tsx
+++ my-app/src/Components/Hero.tsx
@@ -2,7 +2,7 @@ import { HoverBorderGradient } from "./ui/hover-border-gradient"
 
 export function Hero() {
   return (
-    <section id ="home" className="flex flex-col md:flex-row items-center justify-center min-h-screen px-8 md:px-20 bg-[#1E1E1E] text-white">
+    <section id="home" className="flex flex-col md:flex-row items-center justify-center min-h-screen px-8 md:px-20 bg-[#1E1E1E] text-white">
 
       <div className="mb-10 md:mb-0 md:mr-16 flex-shrink-0">
         <img
@@ -10,10 +10,10 @@ export function Hero() {
           alt="Avatar"
           className="w-66 h-66 rounded-full border-[6px] border-[#1E1E1E] shadow-xl"
         />
-      </div> 
+      </div>
 
       <div className="text-center md:text-left max-w-2xl">
-         <h2 className="text-gray-400 text-xl md:text-2xl font-medium">
+        <h2 className="text-gray-400 text-xl md:text-2xl font-medium">
           Hi I’m <span className="text-blue-400">Full-Stack Web Developer</span>
         </h2>
         <h1 className="text-blue-500 text-4xl sm:text-5xl md:text-6xl font-extrabold mt-2 mb-4">
@@ -26,18 +26,17 @@ export function Hero() {
 
         <div className="flex flex-wrap justify-center md:justify-start gap-4">
           <a
-            href="https://docs.google.com/document/d/1Gb1EFxGVPiNGXhGGXQ2lj5Sqed9TM26DDi7Q3WW416s/edit?usp=sharing"
+            href="https://docs.google.com/document/d/1Gb1EFxGVPiNGXhGGXQ2lj5Sqed9TM26DDi7Q3WW416s/view"
             target="_blank"
             rel="noopener noreferrer"
           >
-          <button className="border cursor-pointer border-blue-500 text-blue-500 px-6 py-2.5 rounded-full hover:bg-blue-500 hover:text-black transition font-semibold">
-            Download CV
-          </button>
+            <button className="border cursor-pointer border-blue-500 text-blue-500 px-6 py-2.5 rounded-full hover:bg-blue-500 hover:text-black transition font-semibold">
+              Download CV
+            </button>
           </a>
-          <HoverBorderGradient>
-          <button className=" px-6 py-2.5 rounded-full font-semibold">
+
+          <HoverBorderGradient className="px-6 py-2.5 rounded-full font-semibold">
             Open to work
-          </button>
           </HoverBorderGradient>
         </div>
       </div>
