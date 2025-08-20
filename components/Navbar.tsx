00389693c70d367f9e476c5acc31031a329429c5
daman599 <damanpreetk117@gmail.com>
2025-08-20 20:27:36 +0530

    fixed navbar at top

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,14 +3,16 @@
 const Navbar = ()=>{
   return (
     <>
+    <nav className="fixed top-0 w-full">
     <div className="flex justify-between my-2">
       <div className="ml-50 w-[103px] h-[44px] py-1 px-2 text-[24px] tracking-[3px] hover:text-[#999999] cursor-default">daman</div>
-      <div className="flex justify-between mr-50 py-1 px-2 max-w-52">
+      <div className="flex gap-4 mr-50 py-1 px-2 max-w-64">
           <button className="w-[90px] h-[39px] text-[16px] font-normal tracking-[0px] rounded-[5px]  hover:bg-[#262525] hover:bg-opacity-45 hover:text-[#999999]">Projects</button>
           <button className="w-[90px] h-[39px] text-[16px] font-normal tracking-[0px] rounded-[5px] hover:bg-[#262525] hover:bg-opacity-45 hover:text-[#999999]">About</button>
       </div>
     </div>
-   <div className="w-full h-[1px] bg-[#1C1C1C]"></div>
+    <div className="w-full h-[1px] bg-[#1C1C1C]"></div>
+   </nav>
     </>
   );
 }
