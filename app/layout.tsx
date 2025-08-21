e17bb28735de50ba4458e4f027ff9df9151178cb
daman599 <damanpreetk117@gmail.com>
2025-08-21 22:11:03 +0530

    moved navbar to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({
   subsets: ["latin"], 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
+        <Navbar/>
         {children}
       </body>
     </html>
