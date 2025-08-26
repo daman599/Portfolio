368e99851ab8de19880ea27144d76b99d13879c5
daman599 <damanpreetk117@gmail.com>
2025-08-26 14:01:25 +0530

    few improvements

--- components/About.tsx
+++ components/About.tsx
@@ -1,24 +1,38 @@
+'use client'
+
 import { ArrowLeft } from 'lucide-react';
 import Image from 'next/image';
+import { useRouter } from "next/navigation";
 
 const skills = ["Javascript", "Typrescript", "TailwindCss", "React",
-        "Next.js", "Node.js", "Express", "MongoDB"];
+    "Next.js", "Node.js", "Express", "MongoDB"];
 
 const About = () => {
-    
+    const router = useRouter();
+
     return (
         <>
             <div className='flex ml-96 mr-96 justify-between  mt-24'>
 
-                <div className='flex gap-3 rounded-[5px] hover:bg-[#262525]/45 items-center justify-center group  w-[165px] h-[37px] px-2 py-1.5'>
+                <button onClick={() => {
+                    router.push("/");
+                }}
+                    className='flex gap-3 rounded-[5px] hover:bg-[#262525]/45 items-center justify-center group  w-[165px] h-[37px] px-2 py-1.5 cursor-pointer'>
                     <ArrowLeft className='group-hover:text-white text-[#999999] w-5 transition-colors h-9' />
-                    <button className='font-medium text-[#999999] group-hover:text-white text-[14px] transition-colors'>Back to Projects</button>
-                </div>
-
-                <button className='px-4 py-1.5 rounded-[5px] hover:bg-[#262525]/45 [165px] h-[37px] font-medium text-[#999999]
-      hover:text-white text-[14px]'>
-                    Download Resume
+                    <p className='font-medium text-[#999999] group-hover:text-white text-[14px] transition-colors'>Back to Projects</p>
                 </button>
+
+                <a
+                    href="https://drive.google.com/file/d/11rH4IMGuGgS2_J3OrPWznRo1-6aVCJY-/view?usp=sharing"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <div
+                        className='px-4 py-1.5 rounded-[5px] hover:bg-[#262525]/45 [165px] h-[37px] font-medium text-[#999999]
+        hover:text-white text-[14px] cursor-pointer'>
+                        Download Resume
+                    </div>
+                </a>
             </div>
 
             <div className='ml-24 flex-col'>
