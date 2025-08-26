7ef1a715fd3a3858f8a27c874c3e5615dd12feb5
daman599 <damanpreetk117@gmail.com>
2025-08-26 13:48:04 +0530

    buttons navigate on click

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,24 @@
 'use client'
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const router = useRouter();
+  
   return (
     <>
       <nav className="fixed top-0 w-full backdrop-blur-md bg-[#0a0a0a]/50 z-50">
         <div className="flex justify-between my-2">
           <div className=" ml-50 w-[103px] h-[44px] py-1 px-2 text-[24px] font-normal tracking-[3px] hover:text-[#999999] cursor-default">daman</div>
           <div className="flex gap-4 mr-50 py-1 px-2 max-w-64">
-            <button className="w-[90px] h-[39px] font-normal text-[16px] text-white tracking-[0px] rounded-[5px]  hover:bg-[#262525]/45 hover:text-[#999999] cursor-pointer">Projects</button>
-            <button className="w-[90px] h-[39px] font-normal text-[16px] text-white tracking-[0px] rounded-[5px]  hover:bg-[#262525]/45 hover:text-[#999999] cursor-pointer">About</button>
+            <button onClick={()=>{
+              router.push("/");
+            }}
+            className="w-[90px] h-[39px] font-normal text-[16px] text-white tracking-[0px] rounded-[5px]  hover:bg-[#262525]/45 hover:text-[#999999] cursor-pointer">Projects</button>
+            
+            <button onClick={()=>{
+              router.push("/about");
+            }}
+            className="w-[90px] h-[39px] font-normal text-[16px] text-white tracking-[0px] rounded-[5px]  hover:bg-[#262525]/45 hover:text-[#999999] cursor-pointer">About</button>
           </div>
         </div>
         <div className="w-full h-[1px] bg-[#1C1C1C]"></div>
