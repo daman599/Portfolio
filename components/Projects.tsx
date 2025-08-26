baf12fea905577198158cd75cf733f7d5f586144
daman599 <damanpreetk117@gmail.com>
2025-08-26 15:48:12 +0530

    imgae hover effect

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,11 +5,13 @@ const projects = [
    {
       "project_name": "Funflix",
       "project_type": "Full-stack web app",
+      "project_pic" : "/Funflix.png",
       "month": "June",
    },
    {
       "project_name": "ResumeMatch AI",
       "project_type": "Full-stack web app",
+      "project_pic" : "/ResumeMatch_AI.png",
       "month": "July",
    }
 ]
@@ -24,19 +26,26 @@ const Projects = () => {
          </div>
 
          <div className="flex gap-[42px] ml-[217px] mt-[51px]">
-            {projects.map(({ project_name,project_type, month }, index) => (
+            {projects.map(({ project_name,project_type, project_pic, month }, index) => (
                <div key={index}>
-                 <div className="relative">
+                 <div className="relative cursor-pointer group w-[349px] h-[181px]">
 
                   <Image
                      src="/Project_cover_pic.jpg"
-                     alt="Project Pic"
-                     width={349}
-                     height={181}
-                     className="rounded-[16px]"
+                     alt="Normal"
+                     fill
+                     className="rounded-[16px] object-cover opacity-100 transition-opacity duration-300 group-hover:opacity-0"
                   />
-                  <div className="absolute top-3 left-4 px-3 rounded-full bg-[#201F1F] font-light text-[10px] leading-[30px]">{project_type}</div>
-                  </div>
+
+                   <Image
+                     src={project_pic}
+                     alt="Hover"
+                     fill
+                     className="rounded-[16px] object-cover opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+                  />
+
+                  <div className="absolute top-3 left-4 px-3 rounded-full bg-[#201F1F] font-light text-[10px] leading-[30px] transition-opacity duration-300 opacity-100 group-hover:opacity-0">{project_type}</div>
+               </div>
 
                   <p className="mt-[23px] font-normal text-[22px] ">{project_name}</p>
                   <div className="flex mt-[10px] items-center">
