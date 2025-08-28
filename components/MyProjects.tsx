9c7c9cac4d14ad421f9bf3436e2eca22480a31cb
daman599 <damanpreetk117@gmail.com>
2025-08-29 00:10:34 +0530

    smooth transition

--- components/MyProjects.tsx
+++ components/MyProjects.tsx
@@ -27,49 +27,53 @@ const Projects = () => {
 
    return (
       <>
-         <p className="ml-[217px] text-[48px] font-medium">My Projects</p>
+         <div className="mt-10">
+            <p className="ml-[217px] text-[48px] font-medium">My Projects</p>
 
-         <div className="mt-[23px] text-[16px] font-medium text-[#999999] leading-[25px] ml-[217px]">
-            <p >A small step in my learning journey.</p>
-            <p >Created with passion and attention to detail.</p>
-         </div>
+            <div className="mt-[23px] text-[16px] font-medium text-[#999999] leading-[25px] ml-[217px]">
+               <p >A small step in my learning journey.</p>
+               <p >Created with passion and attention to detail.</p>
+            </div>
 
-         <div className="flex gap-[42px] ml-[217px] mt-[51px]">
-            {projects.map(({ project_name, project_type, project_pic, month, navigateTo }, index) => (
-               <div key={index}>
-                  <button onClick={() => {
-                     router.push(navigateTo);
-                  }}
-                     className="relative cursor-pointer group w-[349px] h-[181px]">
+            <div className="flex gap-[42px] ml-[217px] mt-[51px]">
+               {projects.map(({ project_name, project_type, project_pic, month, navigateTo }, index) => (
+                  <div key={index}>
+                     <button onClick={() => {
+                        router.push(navigateTo);
+                     }}
+                        className="relative cursor-pointer group w-[349px] h-[181px] overflow-hidden rounded-[16px]">
 
-                     <Image
-                        src="/Project_cover_pic.jpg"
-                        alt="Normal"
-                        fill
-                        className="rounded-[16px] object-cover opacity-100 transition-opacity duration-300 group-hover:opacity-0"
-                     />
+                        <Image
+                           src="/Project_cover_pic.jpg"
+                           alt="Normal"
+                           fill
+                           className="object-cover transition-all duration-500 ease-in-out 
+                          opacity-100 scale-105 group-hover:opacity-0 group-hover:scale-100 "
+                        />
 
-                     <Image
-                        src={project_pic}
-                        alt="Hover"
-                        fill
-                        className="rounded-[16px] object-cover opacity-0 transition-opacity duration-300 group-hover:opacity-100"
-                     />
+                        <Image
+                           src={project_pic}
+                           alt="Hover"
+                           fill
+                           className="object-cover transition-all duration-500 ease-in-out 
+                          opacity-0 scale-105 group-hover:opacity-100 group-hover:scale-100
+                        rounded-[16px]"
+                        />
 
-                     <div className="absolute top-3 left-4 px-3 rounded-full bg-[#201F1F] font-light text-[10px] leading-[30px] transition-opacity duration-300 opacity-100 group-hover:opacity-0">{project_type}</div>
-                  </button>
+                        <div className="absolute top-3 left-4 px-3 rounded-full bg-[#201F1F] font-light text-[10px] leading-[30px] transition-opacity duration-300 opacity-100 group-hover:opacity-0">{project_type}</div>
+                     </button>
 
-                  <p className="mt-[23px] font-normal text-[22px] ">{project_name}</p>
-                  <div className="flex mt-[10px] items-center">
-                     <p className="font-normal text-[14px] text-[#999999]" >2025</p>
-                     <Dot className="text-[#999999]" />
-                     <p className="font-normal text-[14px] text-[#999999]">{month}</p>
+                     <p className="mt-[23px] font-normal text-[22px] ">{project_name}</p>
+                     <div className="flex mt-[10px] items-center">
+                        <p className="font-normal text-[14px] text-[#999999]" >2025</p>
+                        <Dot className="text-[#999999]" />
+                        <p className="font-normal text-[14px] text-[#999999]">{month}</p>
+                     </div>
                   </div>
-               </div>
-            ))}
+               ))}
 
+            </div>
          </div>
-
       </>
    );
 }
