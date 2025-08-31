79ea462eac6774a0625cddff5da2020e4673f039
daman599 <damanpreetk117@gmail.com>
2025-08-31 23:34:33 +0530

    myprojects component responsive

--- components/MyProjects.tsx
+++ components/MyProjects.tsx
@@ -6,49 +6,55 @@ import { useRouter } from "next/navigation";
 
 const projects = [
    {
-      "project_name": "ResumeMatch AI",
-      "project_type": "Full-stack web app",
-      "project_pic": "/ResumeMatch_AI.png",
-      "navigateTo": "/project/1",
-      "month": "July",
+      project_name: "ResumeMatch AI",
+      project_type: "Full-stack web app",
+      project_pic: "/ResumeMatch_AI.png",
+      navigateTo: "/project/1",
+      month: "July",
    },
    {
-      "project_name": "Funflix",
-      "project_type": "Full-stack web app",
-      "project_pic": "/Funflix.png",
-      "navigateTo": "/project/2",
-      "month": "June",
-   }
-]
+      project_name: "Funflix",
+      project_type: "Full-stack web app",
+      project_pic: "/Funflix.png",
+      navigateTo: "/project/2",
+      month: "June",
+   },
+];
 
 const Projects = () => {
-
    const router = useRouter();
 
    return (
-      <>
-         <div className="mt-10">
-            <p className="ml-[217px] text-[48px] font-medium">My Projects</p>
+      <div className="mt-10 px-4 sm:px-6 md:px-12 lg:px-24 xl:px-52">
+         <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium">
+            My Projects
+         </p>
 
-            <div className="mt-[23px] text-[16px] font-medium text-[#999999] leading-[25px] ml-[217px]">
-               <p >A small step in my learning journey.</p>
-               <p >Created with passion and attention to detail.</p>
-            </div>
+         <div className="mt-[23px] text-sm md:text-base font-medium text-[#999999] leading-[25px]">
+            <p>A small step in my learning journey.</p>
+            <p>Created with passion and attention to detail.</p>
+         </div>
 
-            <div className="flex gap-[42px] ml-[217px] mt-[51px]">
-               {projects.map(({ project_name, project_type, project_pic, month, navigateTo }, index) => (
-                  <div key={index}>
-                     <button onClick={() => {
-                        router.push(navigateTo);
-                     }}
-                        className="relative cursor-pointer group w-[349px] h-[181px] overflow-hidden rounded-[16px]">
+         <div className="mt-[51px] flex flex-col md:flex-row gap-10 md:gap-[42px]">
+            {projects.map(
+               ({ project_name, project_type, project_pic, month, navigateTo }, index) => (
+                  <div key={index} className="flex flex-col overflow-hidden">
+                     <button
+                        onClick={() => {
+                           router.push(navigateTo);
+                        }}
 
+                        className="
+                         group max-w-[95%] sm:max-w-[320px] md:max-w-[280px] lg:max-w-[349px] 
+                         aspect-[16/9] relative cursor-pointer group w-full sm:w-[349px] h-[181px] 
+                         overflow-hidden rounded-[16px] mx-auto md:mx-0"
+                     >
                         <Image
                            src="/Project_cover_pic.jpg"
                            alt="Normal"
                            fill
                            className="object-cover transition-all duration-500 ease-in-out 
-                          opacity-100 scale-105 group-hover:opacity-0 group-hover:scale-100 "
+                             opacity-100 scale-105 group-hover:opacity-0 group-hover:scale-100"
                         />
 
                         <Image
@@ -56,25 +62,29 @@ const Projects = () => {
                            alt="Hover"
                            fill
                            className="object-cover transition-all duration-500 ease-in-out 
-                          opacity-0 scale-105 group-hover:opacity-100 group-hover:scale-100"
+                             opacity-0 scale-105 group-hover:opacity-100 group-hover:scale-100"
                         />
 
-                        <div className="absolute top-3 left-4 px-3 rounded-full bg-[#201F1F] font-light text-[10px] leading-[30px] transition-opacity duration-300 opacity-100 group-hover:opacity-0">{project_type}</div>
+                        <div className="absolute top-3 left-4 px-3 rounded-full bg-[#201F1F] font-light text-[10px]
+                         leading-[30px] transition-opacity duration-300 opacity-100 group-hover:opacity-0">
+                           {project_type}
+                        </div>
                      </button>
 
-                     <p className="mt-[23px] font-normal text-[22px] ">{project_name}</p>
-                     <div className="flex mt-[10px] items-center">
-                        <p className="font-normal text-[14px] text-[#999999]" >2025</p>
+                     <p className="mt-[23px] font-normal text-lg md:text-[22px] text-center md:text-left">
+                        {project_name}
+                     </p>
+                     <div className="flex mt-[10px] items-center justify-center md:justify-start ">
+                        <p className="font-normal text-sm text-[#999999]">2025</p>
                         <Dot className="text-[#999999]" />
-                        <p className="font-normal text-[14px] text-[#999999]">{month}</p>
+                        <p className="font-normal text-sm text-[#999999]">{month}</p>
                      </div>
                   </div>
-               ))}
-
-            </div>
+               )
+            )}
          </div>
-      </>
+      </div>
    );
-}
+};
 
 export default Projects;
