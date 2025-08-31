b29aea8761f4f1b3d95870a0427a8be780789548
daman599 <damanpreetk117@gmail.com>
2025-08-31 23:11:34 +0530

    updated padding in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,18 +11,21 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 w-full left-0 backdrop-blur-md bg-[#0a0a0a]/50 z-50">
-      <div className="flex justify-between py-1 px-14 md:px-24 lg:px-28 xl:px-[194px]">
+      <div className="flex justify-between py-1 px-2 sm:px-6 md:px-10 lg:px-28 xl:px-[194px]">
 
-        <div className="px-2 py-2 md:py-3 text-base md:text-2xl font-normal tracking-[3px] hover:text-[#999999] cursor-default">daman</div>
+        <div className="px-2 py-2 md:py-3 text-base md:text-2xl font-normal tracking-[3px] hover:text-[#999999] cursor-default">
+          daman
+        </div>
 
-        <div className="flex gap-2 sm:gap-4 p-1 md:p-2">
+        <div className="flex gap-1 sm:gap-3 p-1 md:p-2">
           {buttons.map(({ btn, navigateTo }, index) => (
             <button
               key={index}
               onClick={() => {
                 router.push(navigateTo);
               }}
-              className="w-[70px] h-[30px] md:w-[90px] md:h-[39px] font-normal text-[12px] md:text-base text-white tracking-[0px] rounded-md  hover:bg-[#262525]/45 hover:text-[#999999] cursor-pointer">
+              className="w-[70px] h-[30px] md:w-[90px] md:h-[39px] font-normal text-[12px] md:text-base text-white tracking-[0px] rounded-md hover:bg-[#262525]/45 hover:text-[#999999] cursor-pointer"
+            >
               {btn}
             </button>
           ))}
