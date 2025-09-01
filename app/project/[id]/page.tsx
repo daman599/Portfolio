1d659d72fe5095aaaaa7dae6df1ddbff85315316
daman599 <damanpreetk117@gmail.com>
2025-09-01 19:23:44 +0530

    dynamic project route added

---
+++ app/project/[id]/page.tsx
@@ -0,0 +1,21 @@
+import { projectsData } from "@/lib/projectsData";
+import Project from "@/components/commonComponents/Project";
+
+interface Props {
+   params: { id: string }
+}
+
+export default function ProjectDetailsPage({ params }: Props) {
+
+   const project = projectsData.find((p) => (p.id == params.id));
+
+   if (!project) {
+      return <div className="flex flex-col items-center justify-center min-h-screen px-4">
+         <h1 className="text-red-500 text-xl md:text-2xl lg:text-3xl text-center">
+            Project not found !
+         </h1>
+      </div>
+   }
+
+   return <Project {...project} />
+}
