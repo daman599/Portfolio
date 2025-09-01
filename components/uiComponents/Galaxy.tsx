10c2e32185e3ed064827cc47f08d66eabd15e044
daman599 <damanpreetk117@gmail.com>
2025-09-01 19:53:35 +0530

    few changes

--- components/uiComponents/Galaxy.tsx
+++ components/uiComponents/Galaxy.tsx
@@ -237,8 +237,6 @@ export default function Galaxy({
       gl.clearColor(0, 0, 0, 1);
     }
 
-    let program: Program;
-
     function resize() {
       const scale = 1;
       renderer.setSize(ctn.offsetWidth * scale, ctn.offsetHeight * scale);
@@ -254,7 +252,7 @@ export default function Galaxy({
     resize();
 
     const geometry = new Triangle(gl);
-    program = new Program(gl, {
+    const program = new Program(gl, {
       vertex: vertexShader,
       fragment: fragmentShader,
       uniforms: {
