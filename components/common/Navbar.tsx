c19ca2a9c0d29e44c986d961915a069a68068024
daman599 <damanpreetk117@gmail.com>
2025-11-11 11:42:20 +0530

    few layout improvements

--- components/common/Navbar.tsx
+++ components/common/Navbar.tsx
@@ -4,22 +4,21 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full z-50 backdrop-blur-md bg-[#0a0a0a]/50">
-      <div className="flex justify-between items-center py-1 px-4 sm:px-6 md:px-10 lg:px-20">
-
-        <div className="text-lg md:text-2xl tracking-[3px] text-white hover:text-[#999999] select-none">
+      <div className="flex justify-between items-center py-1 px-4 sm:px-6 md:px-10 lg:px-32">
+        <span className="text-lg tracking-[3px] text-white hover:text-[#999999] select-none">
           daman
-        </div>
+        </span>
 
         <Link
           href="/about"
-          className="px-3 py-1 md:px-4 md:py-2 text-sm md:text-base text-white font-normal rounded-md transition-colors duration-300 hover:bg-[#262525]/45 hover:text-[#999999]"
+          className="px-3 py-2 text-sm text-white font-normal rounded-md transition-colors duration-300 hover:bg-[#262525]/45 hover:text-[#999999]"
         >
           About
         </Link>
       </div>
 
       <div className="w-full h-px bg-[#1C1C1C]" />
-    </nav>
+    </nav >
   );
 };
 
--- components/landingpage/Hero.tsx
+++ components/landingpage/Hero.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
 
         <div className="mt-6 md:mt-9 font-medium text-[#999999] leading-relaxed text-center">
           <div className="text-base sm:text-lg md:text-xl">web developer</div>
-          <div className="text-sm sm:text-lg md:text-xl mt-2 lg:mt-3">
+          <div className="text-sm sm:text-lg md:text-xl mt-1 lg:mt-2">
             with ui/ux designing skills
           </div>
         </div>
@@ -32,7 +32,7 @@ const Hero = () => {
         </div>
       </div >
 
-      <div className="absolute bottom-5 md:bottom-4 lg:bottom-0.5 left-1/2 -translate-x-1/2 w-2 sm:w-3 md:w-3.5 lg:w-4 z-20 h-9 md:h-11 lg:h-14 bg-[#302F2F] rounded-full flex items-start justify-center pt-2 animate-bounce">
+      <div className="absolute bottom-5 md:bottom-4 lg:bottom-0.5 left-1/2 -translate-x-1/2 w-2 sm:w-3 md:w-3.5 z-20 h-9 md:h-11 lg:h-14 bg-[#302F2F] rounded-full flex items-start justify-center pt-2 animate-bounce">
         <div className="bg-[#545050] h-2.5 sm:h-3 md:h-4 lg:h-5 w-0.5 md:w-1 rounded-full"></div>
       </div>
     </>
