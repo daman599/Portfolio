34ed5298de162e88901d7319d4e5fb377cd3c7fe
daman599 <damanpreetk117@gmail.com>
2025-11-11 18:07:35 +0530

    few ui fixes

--- components/landingpage/MyProjects.tsx
+++ components/landingpage/MyProjects.tsx
@@ -9,21 +9,21 @@ import Link from "next/link";
 const Projects = () => {
 
    return (
-      <div className="mt-10 px-4 sm:px-6 md:px-12 lg:px-20 xl:px-48">
+      <div className="mt-10 px-4 sm:px-6 md:px-12 lg:px-24 xl:px-48 md:mb-2">
          <Reveal>
-            <p className="text-2xl sm:text-3xl xl:text-5xl font-medium">
+            <p className="text-2xl xl:text-5xl font-medium">
                My Projects
             </p>
          </Reveal>
 
          <Reveal>
-            <div className="mt-4 xl:mt-6 text-sm md:text-base font-medium text-[#999999] leading-[25px]">
+            <div className="mt-2 xl:mt-6 text-sm xl:text-base font-medium text-[#999999] leading-[25px]">
                <p>A small step in my learning journey.</p>
                <p>Created with passion and attention to detail.</p>
             </div>
          </Reveal>
 
-         <div className="mt-8 lg:mt-10 flex flex-col md:flex-row md:gap-5 lg:gap-8 overflow-x-auto whitespace-nowrap scroll-smooth [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+         <div className="mt-8 lg:mt-9 flex flex-col md:flex-row gap-6 md:gap-4 lg:gap-8 md:overflow-x-auto whitespace-nowrap scroll-smooth [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
             {projectsData.map(
                ({ id, project_name, project_type, project_pic, month }, index) => (
                   <Reveal key={index}>
@@ -58,11 +58,11 @@ const Projects = () => {
                            </div>
                         </Link>
 
-                        <span className="mt-[23px] font-normal text-lg text-center md:text-left">
+                        <span className="mt-3 md:mt-6 font-normal text-lg text-center md:text-left">
                            {project_name}
                         </span>
 
-                        <div className="flex mt-[10px] items-center justify-center md:justify-start ">
+                        <div className="flex mt-1 md:mt-2 items-center justify-center md:justify-start ">
                            <p className="font-normal text-xs xl:text-sm text-[#999999]">2025</p>
                            <Dot className="text-[#999999]" />
                            <p className="font-normal text-xs xl:text-sm text-[#999999]">{month}</p>
