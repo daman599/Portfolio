4a4bf3abeabdcbae84842093cf11188b837bd3b9
daman599 <damanpreetk117@gmail.com>
2025-11-11 12:42:10 +0530

    few ui fixes

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -5,13 +5,13 @@ const Footer = () => {
     <>
       <div className="w-full h-px bg-[#1C1C1C] mt-12 md:mt-20" />
 
-      <div className="flex flex-col md:flex-row justify-between w-full mt-8 max-w-6xl px-6 md:px-10 lg:px-32 xl:px-1 mx-auto gap-9 md:gap-12 lg:gap-20">
+      <div className="flex flex-col md:flex-row justify-between w-full mt-8 max-w-6xl px-6 md:px-10 lg:px-28 xl:px-1 mx-auto gap-9 md:gap-12 lg:gap-20">
         <div className="max-w-md">
           <Reveal>
-            <p className="text-lg md:text-xl lg:text-2xl font-medium leading-snug">
+            <span className="text-lg md:text-xl font-medium leading-snug">
               daman
-            </p>
-            <p className="mt-4 text-[#999999] font-medium text-sm lg:text-base leading-relaxed">
+            </span>
+            <p className="mt-4 text-[#999999] font-medium text-sm leading-relaxed">
               Blending design and technology to create functional, minimal, and
               impactful web apps.
             </p>
@@ -20,10 +20,10 @@ const Footer = () => {
 
         <div>
           <Reveal>
-            <p className="font-normal text-sm md:text-base lg:text-xl leading-[25px]">
+            <span className="font-normal text-sm md:text-base leading-[25px]">
               Contact
-            </p>
-            <div className="mt-4 text-[#999999] font-medium text-sm md:text-base leading-[25px]">
+            </span>
+            <div className="mt-4 text-[#999999] font-medium text-sm leading-[25px]">
               <a
                 href="https://mail.google.com/mail/?view=cm&fs=1&to=damanpreetk117@gmail.com&su=Hello"
                 target="_blank"
@@ -38,10 +38,10 @@ const Footer = () => {
 
         <div>
           <Reveal>
-            <p className="font-normal text-sm md:text-base lg:text-xl leading-[25px]">
+            <p className="font-normal text-sm md:text-base leading-[25px]">
               Connect
             </p>
-            <div className="flex gap-4 md:gap-6 lg:gap-10 mt-3 md:mt-4 font-medium text-sm md:text-base text-[#999999]">
+            <div className="flex gap-4 md:gap-6 lg:gap-10 mt-3 md:mt-4 font-medium text-sm text-[#999999]">
               <a
                 href="https://github.com/daman599"
                 target="_blank"
@@ -63,9 +63,9 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="w-full lg:max-w-[767px] xl:max-w-[1145px] h-px bg-[#1C1C1C] mt-8 mx-auto" />
+      <div className="w-full lg:max-w-[800px] xl:max-w-[1145px] h-px bg-[#1C1C1C] mt-8 mx-auto" />
 
-      <p className="flex items-center justify-center my-4 md:my-6 lg:my-7 text-[10px] md:text-xs lg:text-sm font-light text-[#CCCCCC]">
+      <p className="flex items-center justify-center my-4 md:my-6 lg:my-7 text-[10px] md:text-xs font-light text-[#CCCCCC]">
         © 2025 daman. All rights reserved.
       </p>
     </>
