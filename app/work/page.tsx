d10a344d7756f6dbade3f9eb7a763d3edf2dad69
daman599 <damanpreetk117@gmail.com>
2025-11-17 23:58:36 +0530

    work route added

---
+++ app/work/page.tsx
@@ -0,0 +1,18 @@
+export default function Work() {
+    return (
+        <div className="w-full px-4 sm:px-6 md:px-12 lg:px-28 xl:px-52 mt-14 md:mt-20">
+            <p className="text-base md:text-xl font-medium text-[#19bbb3]">This is what I mean when I say "I love creating UI components".</p>
+
+            <div className="rounded-4xl overflow-hidden shadow-lg size-72">
+                <video
+                    src="/uiComp-videos/navbar.mp4"
+                    autoPlay
+                    loop
+                    muted
+                    className="h-full"
+                />
+            </div>
+
+        </div>
+    );
+}
