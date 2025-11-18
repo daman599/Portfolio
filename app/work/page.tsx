7724b78ae7ad4bd461330c949c1374f96a3dfed4
daman599 <damanpreetk117@gmail.com>
2025-11-18 21:20:43 +0530

    os contributions added

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,3 +1,5 @@
+import { GitMerge } from "lucide-react";
+
 type videosPathType = {
     path: string,
 }
@@ -16,11 +18,20 @@ const videosPath: videosPathType[] = [
     { path: "clickAnimation.mp4" },
 ];
 
+type contributionsTye = {
+    contribution: string,
+}
+
+const Contributions: contributionsTye[] = [
+    { contribution: "fix(ui): resolve overflow bug in filter div with overflow-hidden" },
+    { contribution: "layout improvements" },
+];
+
 export default function Work() {
     return (
         <div className="w-full px-4 sm:px-6 md:px-12 lg:px-28 xl:px-52 mt-14 md:mt-20">
-            <p className="text-base md:text-2xl font-medium text-[#9a9a9a]">
-                This is what I mean when I say, &quot;<span className="text-gray-100">I love creating UI components</span>&quot;
+            <p className="text-xl font-medium text-[#9a9a9a]">
+                This is what I mean when I say, &quot; <span className="text-gray-100">I love creating UI components</span> &quot;
             </p>
             <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:gap-7 rounded-lg md:rounded-xl mt-5 md:mt-10 bg-[#24242485] p-3 md:p-5">
                 {videosPath.map((video, i) => (
@@ -32,6 +43,30 @@ export default function Work() {
                             muted
                             className="w-full h-full object-cover"
                         />
+                    </div>
+                ))}
+            </div>
+
+            <div className="w-full h-[1px] bg-[#1C1C1C] my-10"></div>
+
+            <div className="flex flex-col items-start justify-center gap-5">
+                <span className="text-xl text-[#7c7c7c] underline">Open Source Contributions:-</span>
+
+                {Contributions.map((contri, i) => (
+                    <div key={i} className="flex items-center justify-between w-full">
+                        <div className="flex items-center justify-center gap-2 ">
+                            <div className="px-3 py-2 gap-2 flex items-center justify-center bg-[#542081] rounded-full">
+                                <GitMerge size={18} />
+                                <span className="text-base">Merged</span>
+                            </div>
+                            <span className="text-xl text-[#868686]">
+                                {contri.contribution}
+                            </span>
+                        </div>
+
+                        <a href="https://github.com/Hussain-hamim/DevSync">
+                            <span className="text-lg text-[#40c463]">DevSync</span>
+                        </a>
                     </div>
                 ))}
             </div>
