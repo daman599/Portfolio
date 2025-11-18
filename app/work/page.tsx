ca3105427c2658fa7897da7f5c977e50ebaecfa0
daman599 <damanpreetk117@gmail.com>
2025-11-18 18:50:39 +0530

    bg added behind videos

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -22,9 +22,9 @@ export default function Work() {
             <p className="text-base md:text-2xl font-medium text-[#9a9a9a]">
                 This is what I mean when I say, &quot;<span className="text-gray-100">I love creating UI components</span>&quot;
             </p>
-            <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:gap-7 mt-10">
+            <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 md:gap-7 rounded-lg md:rounded-xl mt-5 md:mt-10 bg-[#24242485] p-3 md:p-5">
                 {videosPath.map((video, i) => (
-                    <div key={i} className="aspect-video overflow-hidden rounded-xl">
+                    <div key={i} className="aspect-video overflow-hidden rounded-lg md:rounded-xl">
                         <video
                             src={`/uiComp-videos/${video.path}`}
                             autoPlay
@@ -35,7 +35,6 @@ export default function Work() {
                     </div>
                 ))}
             </div>
-
         </div >
     );
 }
