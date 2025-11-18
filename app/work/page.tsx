d5b145cdb5294147978c0490a54e4effdcfe5b99
daman599 <damanpreetk117@gmail.com>
2025-11-18 12:02:42 +0530

    few fixes

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,7 +1,9 @@
 export default function Work() {
     return (
         <div className="w-full px-4 sm:px-6 md:px-12 lg:px-28 xl:px-52 mt-14 md:mt-20">
-            <p className="text-base md:text-xl font-medium text-[#19bbb3]">This is what I mean when I say "I love creating UI components".</p>
+            <p className="text-base md:text-xl font-medium text-[#19bbb3]">
+                This is what I mean when I say &quot;I love creating UI components&quot;.
+            </p>
 
             <div className="rounded-4xl overflow-hidden shadow-lg size-72">
                 <video
@@ -12,7 +14,6 @@ export default function Work() {
                     className="h-full"
                 />
             </div>
-
         </div>
     );
 }
